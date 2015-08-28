e32f7daa33b7affd24bc4b0137e77e4418127507
Chrissy Gonzalez <chrissygonzalez@gmail.com>
2015-08-27 21:46:02 -0400

    add two object-oriented cats

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-var cat = document.getElementsByClassName('cat');
-var counter = document.getElementsByClassName('counter');
-var numClicks = 0;
+function Cat(photo, nameElem, counter, catName){
+	this.photo = document.getElementById(photo);
+	this.nameElem = document.getElementById(nameElem);
+	this.nameElem.innerHTML = catName;
+	this.counter = document.getElementById(counter);
+	this.numClicks = 0;
 
-cat[0].addEventListener('click', function(){
-	numClicks++;
-	counter[0].innerHTML = numClicks + ' clicks';
-}, false);
+	var self = this;
+
+	this.photo.addEventListener('click', function(){self.countClicks(self)}, false);
+}
+
+Cat.prototype.countClicks = function(context){
+	context.numClicks++;
+	context.showClicks(context);
+}
+
+Cat.prototype.showClicks = function(context){
+	context.counter.innerHTML = context.numClicks + ' clicks';
+}
+
+var cat1 = new Cat('cat1', 'cat1_name', 'cat1_counter', 'Fluffy');
+var cat2 = new Cat('cat2', 'cat2_name', 'cat2_counter', 'Mittens');
